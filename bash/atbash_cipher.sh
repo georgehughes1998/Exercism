c08872d30cfc080b1c500a8c00128f86429809af
George <hughesgeorgem@gmail.com>
2024-06-05 22:09:22 +0100

    Completed https://exercism.org/tracks/bash/exercises/atbash-cipher

--- bash/atbash_cipher.sh
+++ bash/atbash_cipher.sh
@@ -1,24 +1,46 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+plain=abcdefghijklmnopqrstuvwxyz
+cipher=zyxwvutsrqponmlkjihgfedcba
+stripchars="[ ,.]"
+
+translate() {
+	word=${1,,}
+	word=${word//$stripchars/""}
+	alphabet1=$2
+	alphabet2=$3
+	do_skip=$4
+	
+	for ((i=0;i<${#word};i++)); do
+		letter=${word:$i:1}
+		tail=${alphabet1#*$letter}
+		[[ $letter =~ [a-z] ]] && {
+			index=$((${#alphabet1} - ${#tail} - 1));
+			newletter=${alphabet2:$index:1};
+		} || newletter=$letter
+		newword=$newword$newletter
+		[ $do_skip == true ] && [ $((i%5)) == 4 ] && newword="$newword "
+	done
+
+	echo $newword
+}
+
+encode() {
+	echo $(translate "$1" $plain $cipher true)
+}
+
+decode() {
+	echo $(translate "$1" $cipher $plain false)
+}
+
+case $1 in
+	encode)
+		echo $(encode "$2")
+	;;
+	decode)
+		echo $(decode "$2")
+	;;
+	*)
+		echo "-"
+	;;
+esac
